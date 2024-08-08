77fd76b4c7a5c6599d2b4135b02a8e002fc46961
Mesut <naifaksel@hotmail.com>
2024-08-08 09:51:23 +0300

    Revert "Action type constant sayfasına ayrıldı"
    This reverts commit 7d6ea24e0e95645db228197daa75d9d85a3e56cd.

--- src/Redux/Actions.js
+++ src/Redux/Actions.js
@@ -1,10 +1,9 @@
-import {
-    LOGIN_REQUEST,
-    LOGIN_SUCCESS,
-    LOGIN_FAILURE,
-    LOGOUT_REQUEST,
-    LOGOUT_SUCCESS,
-} from './Constant'
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+
 
 export const loginRequest = (credentials) => ({
     type: LOGIN_REQUEST,
@@ -23,8 +22,6 @@ export const LoginFailure = (error) => ({
 
 export const logoutRequest = () => ({
     type:LOGOUT_REQUEST,
-});
+})
 
-export const logoutSuccess = () => ({
-    type:LOGOUT_SUCCESS,
-});
+export const logoutSuccess
